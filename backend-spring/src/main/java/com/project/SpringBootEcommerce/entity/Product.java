8a4fda9608cc33a3c3367c6b531df649e831bb8e
joebin02 <imstevejoebz@gmail.com>
2025-08-06 23:33:04 +0530

    spring setup

---
+++ backend-spring/src/main/java/com/project/SpringBootEcommerce/entity/Product.java
@@ -0,0 +1,59 @@
+package com.project.SpringBootEcommerce.entity;
+
+
+import jakarta.persistence.*;
+import jdk.jfr.Category;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+
+@Entity
+@Data
+@RequiredArgsConstructor
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id",nullable = false)
+    private ProductCategory category;
+
+    @Column(name = "sku")
+    private String sku;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "unit_price")
+    private BigDecimal unitPrice;
+
+    @Column(name = "imageUrl")
+    private String imageUrl;
+
+    @Column(name = "active")
+    private boolean active;
+
+    @Column(name = "units_in_stock")
+    private int unitsInStock;
+
+    @Column(name = "date_created")
+    @CreationTimestamp
+    private Date dateCreated;
+
+    @Column(name = "last_updated")
+    @UpdateTimestamp
+    private Date lastUpdated;
+}
